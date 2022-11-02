e26ab4ca6564a6cf7959258b6bc826e13b7d3e3a
Sofvi <laitinen.suvi99@gmail.com>
2022-11-02 10:48:22 +0200

    Deletes user password

--- week_two/controllers/userController.js
+++ week_two/controllers/userController.js
@@ -3,12 +3,18 @@ const userModel = require('../models/userModel');
 const users = userModel.users;
 
 const getUsers = (req,res) => {
+  //remove password property from all users
+  users.map(user => {
+    delete user.password;
+    return user;
+  });
   res.json(users);
 }
 
 const getUser = (req,res) => {
   const user = users.filter(user => req.params.userId == user.id)[0];
   if (user) {
+    delete user.password;
     res.json(user);
   } else {
     res.sendStatus(404);
